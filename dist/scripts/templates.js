e6bd5ffb7d6eefb6f635709277ff4a134eae00fa
Alex Howard <thezanke@gmail.com>
2016-01-17 04:12:07 -0500

    Going to bed, need to push, lol

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -15,7 +15,7 @@ this["zframe"]["Templates"]["application"] = Handlebars.template({"1":function(d
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<div class=\"mainContainer\">\n  <div class=\"menuContainer\">\n    <ul class=\"menuList\">\n      <h1>This is a test</h1>\n"
+  return "<div class=\"mainContainer\">\n  <div class=\"menuContainer\">\n    <ul class=\"menuList\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.menuItems : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "    </ul>\n  </div>\n</div>\n";
 },"useData":true});
