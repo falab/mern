8f3fa38feacb78b90b0e0840224925a6824be613
Alex Howard <alex@moser-inc.com>
2016-04-30 16:27:07 -0400

    Fixing import

--- app/application.jsx
+++ app/application.jsx
@@ -1,4 +1,6 @@
-import ReactDOM from 'react-dom';
-import AppRoutes from './components/AppRoutes';
+import React from 'react';
 
-ReactDOM.render(AppRoutes, document.getElementById('app'));
+import AppRoutes from './components/AppRoutes';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<AppRoutes />, document.getElementById('app'));
--- app/components/AppRoutes.jsx
+++ app/components/AppRoutes.jsx
@@ -8,7 +8,7 @@ import PortfolioPage from './PortfolioPage';
 import BlogPage from './BlogPage';
 import NotFoundPage from './NotFoundPage';
 
-export default class Main extends React.Component {
+export default class AppRoutes extends React.Component {
   render() {
     return (
       <Router history={browserHistory}>
