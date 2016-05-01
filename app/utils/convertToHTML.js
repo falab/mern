5109d98e18fb56042f8b898f2e9661d1798d4f90
Alex Howard <alex@moser-inc.com>
2016-04-30 22:07:05 -0400

    Working on convertToHTML

--- app/utils/convertToHTML.js
+++ app/utils/convertToHTML.js
@@ -1,6 +1,32 @@
 import { convertToRaw } from 'draft-js';
 
+const BLOCK_TYPES = {
+  unstyled: ['<p>', '</p>'],
+  'header-one': ['<h1>', '</h1>'],
+  'header-two': ['<h2>', '</h2>'],
+  'header-three': ['<h3>', '</h3>'],
+  'header-four': ['<h4>', '</h4>'],
+  'header-five': ['<h5>', '</h5>'],
+  'header-size': ['<h6>', '</h6>'],
+  blockquote: ['<blockquote>', '</blockquote>'],
+};
+
+const STYLE_TYPES = {
+  BOLD: ['<b>', '</b>'],
+  ITALIC: ['<em>', '</em>'],
+  UNDERLINE: ['<u>', '</u>'],
+  CODE: ['<span class="mono">', '</span>'],
+};
+
 export default function convertToHTML(contentState) {
   const rawContent = convertToRaw(contentState);
-  console.log(rawContent);
+
+  let retHTML = '';
+
+  rawContent.blocks.forEach(({ text, type, inlineStyleRanges: styles }) => {
+    const [blockOpenTag, blockCloseTag] = BLOCK_TYPES[type];
+    retHTML += `${blockOpenTag}${text}${blockCloseTag}`;
+  });
+
+  return retHTML;
 }
