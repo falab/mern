72fdff79725b942acb93a2049f61451f770c6757
Alex Howard <alex@moser-inc.com>
2016-05-02 08:34:37 -0400

    Add sanitize method for potential use

---
+++ app/utils/sanatize.js
@@ -0,0 +1,26 @@
+const tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+const tagOrComment = new RegExp([
+  '<(?:',
+  // Comment body.
+  '!--(?:(?:-*[^->])*--+|-?)',
+  // Special "raw text" elements whose content should be elided.
+  `|script\\b${tagBody}>[\\s\\S]*?</script\\s*`,
+  `|style\\b${tagBody}>[\\s\\S]*?</style\\s*`,
+  // Regular name
+  '|/?[a-z]',
+  tagBody,
+  ')>',
+].join(''), 'gi');
+
+export function sanitize(_html) {
+  let oldHtml;
+  let html = _html;
+
+  do {
+    oldHtml = html;
+    html = html.replace(tagOrComment, '');
+  } while (html !== oldHtml);
+
+  return html.replace(/</g, '&lt;');
+}
