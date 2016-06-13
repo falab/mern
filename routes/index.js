daa1d23ef39d268bfa4e70e1c14ce395aa8bd4f8
Alex Howard <alex@moser-inc.com>
2016-06-12 23:16:06 -0400

    removing extra code left over from merge

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = new express.Router();
-
-router.use('/blog', require('./blog'));
-router.use('/socialIcons', require('./socialIcons'));
-
-module.exports = router;
